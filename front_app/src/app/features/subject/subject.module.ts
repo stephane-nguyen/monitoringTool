435aa5c3ac8eeb995f409af21823d9ee6ff22121
Stéphane <stepdh01@gmail.com>
2022-12-26 20:23:04 +0100

    add edit and add dialog component

--- front_app/src/app/features/subject/subject.module.ts
+++ front_app/src/app/features/subject/subject.module.ts
@@ -3,18 +3,18 @@ import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { SubjectRoutingModule } from './subject-routing.module';
 import { SubjectListComponent } from './subject-list/subject-list.component';
-import { SubjectAddComponent } from './subject-add/subject-add.component';
-import { SubjectDialogFormComponent } from './subject-dialog-form/subject-dialog-form.component';
+import { SubjectDialogAddComponent } from './subject-dialog-add/subject-dialog-add.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { SubjectDialogEditComponent } from './subject-dialog-edit/subject-dialog-edit.component';
 
 @NgModule({
   declarations: [
     SubjectListComponent,
-    SubjectAddComponent,
-    SubjectDialogFormComponent,
+    SubjectDialogAddComponent,
+    SubjectDialogEditComponent,
   ],
   imports: [
     SubjectRoutingModule,
